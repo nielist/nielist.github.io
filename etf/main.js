00edfdcbefc71d9975eaa6d07e4de9af034f82ea
nielist <nielist@gmail.com>
2020-10-21 15:06:16 +0800

    Update main.js

--- etf/main.js
+++ etf/main.js
@@ -328,7 +328,7 @@ function onClickAddNewStock() {
       html += '<input type="number" step="0.01" value="0" data-value="0" class="form-control" id="' + id + '">';
       html += '</div>';
 
-      $('input[type=number].form-control').last().parent().after(html);
+      $('#form-first-element').after(html);
     }
   }
 
