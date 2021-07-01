881e9c88c9d03c362eea2866eff6435a43c97df5
nielist <nielist@gmail.com>
2021-07-01 21:00:59 +0800

    Align naming convention

--- artbat/artbat.js
+++ artbat/artbat.js
@@ -6,38 +6,40 @@ const COLOR_LIGHTBLUE = "#00ABFF";
 const COLOR_BLUE = "#000085";
 
 var url_prefix = ""; //"https://nielist.github.io/artbat/";
+
 var timer_state = "reset";
 var timer_minutes_default = 20;
 var timer_seconds = 60 * timer_minutes_default;
 var timer_object = null;
-var dict_words_adjectives = [];
-var dict_words_nouns = [];
-var pending_spin_colors = [COLOR_PINK, COLOR_RED, COLOR_YELLOW, COLOR_GREEN, COLOR_LIGHTBLUE, COLOR_BLUE];
-var pending_spin_words_adjectives = [];
-var pending_spin_words_noun = [];
-var active_word_adjective = "";
-var active_word_noun = "";
-var spin_words_timer_object = null;
-var artbat_pastpaper = [];
+
+var dict_colors = [COLOR_PINK, COLOR_RED, COLOR_YELLOW, COLOR_GREEN, COLOR_LIGHTBLUE, COLOR_BLUE];
+var dict_adjectives = [];
+var dict_nouns = [];
+var dict_pastpaper = [];
+var pending_spin_adjectives = [];
+var pending_spin_nouns = [];
+var active_adjective = "";
+var active_noun = "";
+var spin_timer_object = null;
 
 function init() {
   updateTimer("reset");
-  $("#timer-btn-start").click(function(event){
+  $("#btn_timer_start").click(function(event){
     if(!event.detail || event.detail == 1){
       updateTimer("start");
     }
   });
-  $("#timer-btn-continue").click(function(event){
+  $("#btn_timer_continue").click(function(event){
     if(!event.detail || event.detail == 1){
       updateTimer("continue");
     }
   });
-  $("#timer-btn-pause").click(function(event){
+  $("#btn_timer_pause").click(function(event){
     if(!event.detail || event.detail == 1){
       updateTimer("pause");
     }
   });
-  $("#timer-btn-reset").click(function(event){
+  $("#btn_timer_reset").click(function(event){
     if(!event.detail || event.detail == 1){
       updateTimer("reset");
     }
@@ -45,26 +47,26 @@ function init() {
 
   getWords();
   showWords("???", "???");
-  $("#words-btn-spin").click(function(event){
+  $("#btn_spin").click(function(event){
     //activate on first click only to avoid hiding again on multiple clicks
     //execute only once on multiple clicks
     if(!event.detail || event.detail == 1){
-      $("#words-btn-spin").addClass("disabled");
+      $("#btn_spin").addClass("disabled");
       shuffleWords();
       let counter = 10;
       let count = counter;
-      spin_words_timer_object = setInterval(function() {
+      spin_timer_object = setInterval(function() {
         count--;
         if (count <= 0) {
-          clearInterval(spin_words_timer_object);
-          $("#words-btn-spin").removeClass("disabled");
+          clearInterval(spin_timer_object);
+          $("#btn_spin").removeClass("disabled");
         }
-        showWords(dict_words_adjectives[counter-count], dict_words_nouns[counter-count]);
+        showWords(dict_adjectives[counter-count], dict_nouns[counter-count]);
       }, 100);
     }
   });
 
-  getArtbatPastpaperCsv();
+  getPastpaperCsv();
 }
 
 function onClickAddNewColor() {
@@ -93,33 +95,33 @@ function updateTimer(t_state) {
   switch (timer_state) {
     case "start":
       setTimer();
-      $("#timer-btn-start").addClass("disabled hide");
-      $("#timer-btn-continue").addClass("disabled hide");
-      $("#timer-btn-pause").removeClass("disabled hide");
-      $("#timer-btn-reset").removeClass("disabled hide");
+      $("#btn_timer_start").addClass("disabled hide");
+      $("#btn_timer_continue").addClass("disabled hide");
+      $("#btn_timer_pause").removeClass("disabled hide");
+      $("#btn_timer_reset").removeClass("disabled hide");
       break;
     case "continue":
       setTimer();
-      $("#timer-btn-start").addClass("disabled hide");
-      $("#timer-btn-continue").addClass("disabled hide");
-      $("#timer-btn-pause").removeClass("disabled hide");
-      $("#timer-btn-reset").removeClass("disabled hide");
+      $("#btn_timer_start").addClass("disabled hide");
+      $("#btn_timer_continue").addClass("disabled hide");
+      $("#btn_timer_pause").removeClass("disabled hide");
+      $("#btn_timer_reset").removeClass("disabled hide");
       break;
     case "pause":
       clearTimer();
-      $("#timer-btn-start").addClass("disabled hide");
-      $("#timer-btn-continue").removeClass("disabled hide");
-      $("#timer-btn-pause").addClass("disabled hide");
-      $("#timer-btn-reset").removeClass("disabled hide");
+      $("#btn_timer_start").addClass("disabled hide");
+      $("#btn_timer_continue").removeClass("disabled hide");
+      $("#btn_timer_pause").addClass("disabled hide");
+      $("#btn_timer_reset").removeClass("disabled hide");
       break;
     case "reset":
     default:
       timer_seconds = 60 * timer_minutes_default;
       clearTimer();
-      $("#timer-btn-start").removeClass("disabled hide");
-      $("#timer-btn-continue").addClass("disabled hide");
-      $("#timer-btn-pause").addClass("disabled hide");
-      $("#timer-btn-reset").addClass("disabled hide");
+      $("#btn_timer_start").removeClass("disabled hide");
+      $("#btn_timer_continue").addClass("disabled hide");
+      $("#btn_timer_pause").addClass("disabled hide");
+      $("#btn_timer_reset").addClass("disabled hide");
       break;
   }
 }
@@ -143,46 +145,46 @@ function setTimer() {
 function showTimerMessage() {
   let minutes = Math.floor((timer_seconds % (60 * 60)) / 60);
   let seconds = Math.floor((timer_seconds % 60));
-  $("#timer-txt").html("" + (minutes<10?'0':'') + minutes + ":" + (seconds<10?'0':'') + seconds);
+  $("#div_timer_display").html("" + (minutes<10?'0':'') + minutes + ":" + (seconds<10?'0':'') + seconds);
   if (timer_seconds <= 0) {
-    $("#timer-txt").html("finish!");
+    $("#div_timer_display").html("finish!");
   }
 }
 
 function getWords() {
   $.getJSON(url_prefix + "data/adjectives.json", function(result){
-    dict_words_adjectives = result.data;
+    dict_adjectives = result.data;
   });
   $.getJSON(url_prefix + "data/nouns.json", function(result){
-    dict_words_nouns = result.data;
+    dict_nouns = result.data;
   });
 }
 function shuffleWords() {
-  dict_words_adjectives.sort(function() { return 0.5 - Math.random() });
-  dict_words_nouns.sort(function() { return 0.5 - Math.random() });
+  dict_adjectives.sort(function() { return 0.5 - Math.random() });
+  dict_nouns.sort(function() { return 0.5 - Math.random() });
 }
 function showWords(adjective, noun) {
-  var active_word_adjective = adjective;
-  var active_word_noun = noun;
-  $("#words-txt-adjectives").html(active_word_adjective);
-  $("#words-txt-nouns").html(active_word_noun);
+  var active_adjective = adjective;
+  var active_noun = noun;
+  $("#lbl_adjt").html(active_adjective);
+  $("#lbl_noun").html(active_noun);
 }
 
-function getArtbatPastpaperCsv() {
+function getPastpaperCsv() {
   $.ajax({
     type: "GET",
     url: url_prefix + "data/pastpaper.csv",
     dataType: "text",
     success: function(response)
     {
-      artbat_pastpaper = $.csv.toObjects(response);
-      showArtbatPastpaperTable(artbat_pastpaper);
+      dict_pastpaper = $.csv.toObjects(response);
+      showPastpaperTable(dict_pastpaper);
     }
   });
 }
 
-function showArtbatPastpaperTable(data) {
-  var html = '<table class="table table-condensed table-hover table-striped" id="table-pastpaper">';
+function showPastpaperTable(data) {
+  var html = '<table class="table table-condensed table-hover table-striped" id="tbl_pastpaper">';
 
   if(typeof(data[0]) === 'undefined') {
     return null;
@@ -213,6 +215,6 @@ function showArtbatPastpaperTable(data) {
     });
     html += '</tbody>';
     html += '</table>';
-    $('#wrapper-table-pastpaper').append(html);
+    $('#div_pastpaper_display').append(html);
   }
 }
