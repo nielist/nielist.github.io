263731868b8b1847fab5dc3a3ab0dca86f089b38
nielist <nielist@gmail.com>
2022-05-29 19:21:51 +0800

    Add files via upload

---
+++ alert-translate/translate.js
@@ -0,0 +1,34 @@
+var url_prefix = "https://nielist.github.io/alert-translate/";
+
+var dictionary_list = [];
+
+$(document).ready(function(){
+  getDictionaryCsv();
+  $("#source-text").change(function(e){
+    e.preventDefault();
+    onChangeSourceText();
+  });
+});
+
+function getDictionaryCsv() {
+  $.ajax({
+    type: "GET",
+    url: url_prefix + "dictionary.csv",
+    dataType: "text",
+    success: function(response)
+    {
+      let csv_obj = $.csv.toObjects(response);
+      if (csv_obj.length > 0) {
+        dictionary_list = csv_obj;
+		dictionary_list.sort((a, b) => (a.en.length > b.en.length) ? 1 : -1);
+		console.log(dictionary_list);
+      }
+    }
+  });
+}
+
+function onChangeSourceText() {
+  let source_text = $("#source-text").val();
+  let re = new RegExp("a|b", "i");
+  $("#target-text").val(source_text);
+}
